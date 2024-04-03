c6b998ab0ea81af4bfa51bd912f429e2d276a89d
GenesysR-dev <genesys.rondon@alura.com.br>
2024-04-02 21:20:34 -0300

    aula 01

--- src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
@@ -8,21 +8,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScreenmatchApplication implements CommandLineRunner {
+public class ScreenmatchApplication {
 
-	@Autowired
-	private SerieRepository repository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenmatchApplication.class, args);
 	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		Principal principal = new Principal(repository);
-		principal.muestraElMenu();
-
-
-
-
-	}
 }
+
---
+++ src/main/java/com/aluracursos/screenmatch/ScreenmatchApplicationConsola.java
@@ -0,0 +1,28 @@
+//package com.aluracursos.screenmatch;
+//
+//import com.aluracursos.screenmatch.principal.Principal;
+//import com.aluracursos.screenmatch.repository.SerieRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class ScreenmatchApplicationConsola implements CommandLineRunner {
+//
+//	@Autowired
+//	private SerieRepository repository;
+//	public static void main(String[] args) {
+//		SpringApplication.run(ScreenmatchApplicationConsola.class, args);
+//	}
+//
+//	@Override
+//	public void run(String... args) throws Exception {
+//		Principal principal = new Principal(repository);
+//		principal.muestraElMenu();
+//
+//
+//
+//
+//	}
+//}
