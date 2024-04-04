8450874be50ac561f90043ff3c3a13238c5dc76d
GenesysR-dev <genesys.rondon@alura.com.br>
2024-04-04 17:09:24 -0300

    desafío manos en la masa

--- src/main/java/com/aluracursos/screenmatch/repository/SerieRepository.java
+++ src/main/java/com/aluracursos/screenmatch/repository/SerieRepository.java
@@ -22,4 +22,7 @@ public interface SerieRepository extends JpaRepository<Serie,Long> {
 
     @Query("SELECT e FROM Serie s JOIN s.episodios e WHERE s.id = :id AND e.temporada = :numeroTemporada")
     List<Episodio> obtenerTemporadasPorNumero(Long id, Long numeroTemporada);
+
+    @Query("SELECT e FROM Serie s JOIN s.episodios e WHERE s = :serie ORDER BY e.evaluacion DESC LIMIT 5")
+    List<Episodio> topEpisodiosPorSerie(Serie serie);
 }
