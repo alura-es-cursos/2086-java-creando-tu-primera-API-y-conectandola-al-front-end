5789dcd0d712acde5c35aa703fa34932bd1dd99c
GenesysR-dev <genesys.rondon@alura.com.br>
2024-04-04 16:17:27 -0300

    corrigiendo anotación

--- src/main/java/com/aluracursos/screenmatch/controller/SerieController.java
+++ src/main/java/com/aluracursos/screenmatch/controller/SerieController.java
@@ -11,7 +11,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/series")
 public class SerieController {
     @Autowired
     private SerieRepository repository;
