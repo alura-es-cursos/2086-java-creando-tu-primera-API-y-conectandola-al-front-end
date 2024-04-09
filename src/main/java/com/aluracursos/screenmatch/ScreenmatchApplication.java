9dacc7d020fd25d2a626cddc3949661611d3004b
GenesysR-dev <genesys.rondon@alura.com.br>
2024-04-09 14:54:07 -0300

    corrigiendo imports no utilizados

--- src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
@@ -1,9 +1,5 @@
 package com.aluracursos.screenmatch;
 
-import com.aluracursos.screenmatch.principal.Principal;
-import com.aluracursos.screenmatch.repository.SerieRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
